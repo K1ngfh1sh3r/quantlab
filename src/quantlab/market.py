8502029989ee20a6d10592a55ab375778fb8749b
singe <paul.josse@ecole2600.com>
2026-07-10 20:28:27 +0200

    implement data market downloader

--- src/quantlab/market.py
+++ src/quantlab/market.py
@@ -1,9 +1,12 @@
 import yfinance as yf
 from datetime import date
+import pandas
 
-
-help(yf.download)
-def download_market_data(ticker:str,start:date,end:date):
+def download_market_data(
+    ticker:str,
+    start:date,
+    end:date
+    ) -> pandas.DataFrame :
     """
     Download historical market data
     
@@ -21,4 +24,6 @@ def download_market_data(ticker:str,start:date,end:date):
         end=end,
         progress=False)
     
+    if market_data.columns.nlevels > 1:
+        market_data.columns = market_data.columns.droplevel(1)
     return market_data
