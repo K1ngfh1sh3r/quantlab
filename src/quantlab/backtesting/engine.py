4ff241ca99c4cb7ccaae72e5f275b3536724aad1
K1ngf1sher <josse.paul744@gmail.com>
2026-07-22 03:43:20 +0200

    backtesting

--- src/quantlab/backtesting/engine.py
+++ src/quantlab/backtesting/engine.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class BacktestEngine:
     """
     Basic backtesting engine.
@@ -55,3 +57,47 @@ class BacktestEngine:
                         price: float
         ) -> float:
         return self.cash + (self.shares * price)
+    
+    def run(self,
+            data: pd.DataFrame,
+            price_column: str,
+            signal_column: str
+        )-> pd.DataFrame:
+        """
+        Executes a backtest using trading signals.
+
+        Args:
+            data:
+                DataFrame containing market data and signals.
+                
+            price_column:
+                Column containing asset prices.
+
+            signal_column:
+                Column containing trading signals.
+
+        Returns:
+            DataFrame containing portfolio evolution.
+        """
+        result = data.copy()
+        
+        portfolio_values = []
+        
+        for _, row in result.iterrows():
+            
+            price = row[price_column]
+            signal = row[signal_column]
+            
+            if signal == 1:
+                self.buy(price,1)
+            
+            elif signal == -1:
+                self.sell(price,1)
+                
+            portfolio_values.append(
+                self.portfolio_value(price)
+            )
+            
+        result["Portfolio_Value"] = self.portfolio_values
+        
+        return result
